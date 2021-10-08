3e1cc51006c6cd20454df00d433c3237965bc787
nikhilgopalam <nikhil@usestir.com>
2021-10-07 23:22:13 -0700

    Created card component and set up buyer page

---
+++ frontend/components/card/index.tsx
@@ -0,0 +1,88 @@
+import { Container, Center, Text, Flex, Badge, Image, Box, Spacer, Heading, Button } from '@chakra-ui/react'
+
+export function Card({
+    image,
+    title,
+    originalProject,
+    price,
+}: {
+    image: string;
+    title: string;
+    originalProject: string;
+    price: number;
+}) {
+    return (
+      
+      <Box maxWidth={52} borderWidth="1px" borderRadius="lg" overflow="hidden">
+        <Center p="8px" pb="12px">
+          <Image borderRadius="4px" src={`../../${image}`} alt="Image of Derivative NFT" />
+        </Center>
+        <Box px="8px" mb="4px">
+          <Flex>
+            <Text 
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              ml="2px"
+            >Title</Text>
+            <Spacer />
+            <Text
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              mr="2px"
+            >Price</Text>
+          </Flex>
+          <Flex>
+            <Text
+              fontWeight="bold"
+              ml="2px"
+            >
+              {title}
+            </Text>
+            <Spacer />
+            <Image w="8px" mr="2px" src="../../ethereum-logo.svg"/>
+            <Text>
+              {(price / 100).toFixed(2)}
+            </Text>
+          </Flex>
+          <Text
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            ml="2px"
+          >
+            Created for: {originalProject}
+          </Text>
+          </Box>
+      </Box>
+    //     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+    //   <Image src={`../../${image}`} alt="Image of Derivative NFT" />
+
+    //   <Box p="6">
+    //     <Box d="flex" alignItems="baseline">
+    //       <Badge borderRadius="full" px="2" colorScheme="teal">
+    //         New
+    //       </Badge>
+    //     </Box>
+
+    //     <Box
+    //       mt="1"
+    //       fontWeight="semibold"
+    //       as="h4"
+    //       lineHeight="tight"
+    //       isTruncated
+    //     >
+    //       {title}
+    //     </Box>
+
+    //     <Box>
+    //       {(price / 100).toFixed(2)}
+    //     </Box>
+    //   </Box>
+    // </Box>
+    )
+}
--- frontend/pages/buyer.tsx
+++ frontend/pages/buyer.tsx
@@ -1,6 +1,8 @@
    
 import React from 'react';
 import Link from 'next/link'
+import { Card } from '../components/card';
+import { SimpleGrid, Container, Text } from '@chakra-ui/react'
 
 interface Props {
     title?: string;
@@ -8,14 +10,33 @@ interface Props {
 }
 
 function BuyerPage(props: Props) {
+
+    // TODO: Pull info from IPFS here
+    // TODO: Format info from IPFS here
+    
+    let NFTList: any = []
+    // TODO: Change filler function once we actually pull info from IPFS
+    // for all NFTs in a project
+    for (let i = 0; i < 11; i++) {
+        NFTList.push(
+            <Card image="Rectangle_38_gradient.png" title="GradientPunks" originalProject="CryptoPunk" price={400} />
+        )
+    }
+
     return (
-    	<div>
-    	<p>Should render the title {props.title}</p> 
-    		<Link href="/buyer/[tokenid]" as={`/buyer/${props.tokenid}`}> 
-    			<a>Let's Pretend this is a derivative: {props.tokenid} </a>
-    		</Link>
-    	</div>
-    	);
+        <Container maxW="container.md">
+          <Text fontWeight="bold" fontSize="6xl"> Thin Mints </Text>
+          <div>
+            <p>Should render the title {props.title}</p> 
+            <Link href="/buyer/[tokenid]" as={`/buyer/${props.tokenid}`}> 
+              <a>Let's Pretend this is a derivative: {props.tokenid} </a>
+            </Link>
+          </div>
+          <SimpleGrid py="24px" columns={{sm: 2, md: 3}} spacing="40px">
+              {NFTList}
+          </SimpleGrid>
+        </Container>
+    );
 }
 
 export const getServerSideProps = async () => {
