d3ce96b18b9bc95bee4e50e15d79e126b9fb1cbd
jierlich <jierlich@umich.edu>
2021-10-09 15:52:10 -0400

    [FEAT] add deployment info to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const environment = require('./env')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${environment.alchemyAPIKey}`,
+      accounts: [environment.deployerPK]
+    }
+  },
+  etherscan: {
+    apiKey: environment.etherscanAPIKey
+  }
 };
